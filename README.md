2ddc2aca139cea3f971b37d0d6bc3a678dd71940
Pierre Delaunay <pierre@delaunay.io>
2022-04-11 16:28:32 -0400

    Add cross compilation build

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 Lua
 ===
 
+Standalone lua installation engineered for a multi arch Lmod installation.
+
+Features
+--------
+
+* Statically include
+    * lfs
+    * lpeg
+    * luaposix
+
+* build using alpine linux 
+    * statically linked against musl (no dependency on any system libraries)
+
+* Cross compiled
+    * x86_64
+    * ppc64le
+    * riscv64
+    * aarch64
--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
+#!/bin/sh
 
-base=$(pwd)/modules
+origin=$(pwd)
+base=$origin/modules
 
-mkdir -p $base/lua/bin
+arch=${1:-x86_64}
 
 rm -rf build
 meson subprojects download
 
-meson setup -Dprefix=$base/lua     \
-            -Dbindir='bin'         \
-            -Dbuildtype=minsize    \
-            -Db_staticpic=false    \
-            -Db_ndebug=true        \
-            -Db_lto=true           \
+version=$(cd subprojects/lua && git describe --tags --abbrev=0)
+loc=$base/lua/$version/$arch
+
+mkdir -p $loc/bin
+
+# Setup cross compilation
+cd $origin/lua/buildroot/$arch
+make -C $origin/subprojects/buildroot-2021.08 V=1 O=$(pwd)
+export PATH="$(pwd)/host/bin:$PATH"
+
+cd $origin
+
+# Standard Meson build
+meson setup -Dprefix=$loc       \
+            -Dbindir='bin'      \
+            -Dbuildtype=minsize \
+            -Db_staticpic=false \
+            -Db_ndebug=true     \
+            -Db_lto=true        \
             build .       
 
 meson compile -C build
